c60c4f6e5f1618094482af7194997579155948d3
Rokas Kasperavicius <rokas@Rokass-MacBook-Pro.local>
2023-02-25 22:00:30 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const puppeteer = require("puppeteer");
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/youtube", async (req, res) => {
+  const search = req.query.search;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(`https://www.youtube.com/results?search_query=${search}`);
+
+  const aTag = await page.waitForSelector(
+    "ytd-app > #content > ytd-page-manager > ytd-search > #container > ytd-two-column-search-results-renderer > #primary > ytd-section-list-renderer > #contents > ytd-item-section-renderer > #contents > ytd-video-renderer:nth-child(1) a"
+  );
+  const href = await aTag.evaluate((el) => el.href);
+
+  console.log(href);
+
+  if (!href) {
+    res.json({
+      id: "",
+    });
+  }
+
+  const matches = href.match(/v=([^&]*)/);
+
+  res.json({
+    id: matches[1],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
